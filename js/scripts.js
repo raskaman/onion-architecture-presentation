c01a27599c007e2610a759301f2239aac1b65fd1
John Mondragon <j-mondragon@live.com>
2017-10-12 13:10:25 -0500

    adding presentation

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="jquery-2.1.3.min.js" />
+
+$(document).ready(function () {
+
+    $('.step').each(function () {
+        $('<a>', {
+            text: $(this).attr('data-desc'),
+            href: '#' + $(this).attr('id')
+        }).appendTo('#navigation');
+        $('<br>').appendTo('#navigation');
+    });
+
+    $('#menu').click(function () {
+        $('#navigation').toggle('slow');
+    });
+
+});
